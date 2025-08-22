ef4320421a5888d00e47305a58bb5b8073c3ee21
Rohith <rohithchandolu3064@gmail.com>
2025-08-22 18:07:00 +0530

    Fix hydration mismatch by removing duplicate __next div

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${sourceSans.variable} antialiased`}>
       <body className={sourceSans.className}>
-        <div id="__next"></div>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
           <Toaster />
